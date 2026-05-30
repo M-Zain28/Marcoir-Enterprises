b47cbc66ac0b707fd96b697b1811131fddbba575
Muhammad Zain Akmal <76583481+M-Zain28@users.noreply.github.com>
2026-05-30 16:07:27 +0500

    Update vite.config.ts
    added port and basepath

--- artifacts/marcoir-website/vite.config.ts
+++ artifacts/marcoir-website/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
+/*
 const rawPort = process.env.PORT;
 
 if (!rawPort) {
@@ -11,6 +11,9 @@ if (!rawPort) {
     "PORT environment variable is required but was not provided.",
   );
 }
+*/
+const port = Number(process.env.PORT || 3000);
+const basePath = process.env.BASE_PATH || "/";
 
 const port = Number(rawPort);
 
